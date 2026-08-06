c26e8d95f22873b1bb4ed518c5713928e160276e
muhammadsadiqabubakar09 <muhammadsadiqabubakar09@gmail.com>
2026-08-06 12:43:17 +0100

    Upgrade Multi-Timeframe Engine V2

--- multi_timeframe.py
+++ multi_timeframe.py
@@ -1,28 +1,37 @@
 from binance_api import get_candles
 from indicators import add_indicators
 
-
-TIMEFRAMES = [
-    "5m",
-    "15m",
-    "1h"
-]
+TIMEFRAMES = ["5m", "15m", "1h"]
 
 
 def analyze_timeframe(symbol, interval):
 
     df = get_candles(symbol, interval)
-
     df = add_indicators(df)
 
     last = df.iloc[-1]
 
     return {
-        "timeframe": interval,
+        "close": last["close"],
+
         "ema20": last["ema20"],
         "ema50": last["ema50"],
+
         "rsi": last["rsi"],
-        "price": last["close"],
+
+        "macd": last["macd"],
+        "macd_signal": last["macd_signal"],
+
+        "adx": last["adx"],
+
+        "atr": last["atr"],
+
+        "bb_upper": last["bb_upper"],
+        "bb_middle": last["bb_middle"],
+        "bb_lower": last["bb_lower"],
+
+        "volume": last["volume"],
+        "volume_sma": last["volume_sma"]
     }
 
 
@@ -31,7 +40,9 @@ def analyze_symbol(symbol):
     result = {}
 
     for tf in TIMEFRAMES:
-
         result[tf] = analyze_timeframe(symbol, tf)
 
     return result
+
+
+       
