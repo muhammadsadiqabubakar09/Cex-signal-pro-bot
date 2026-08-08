c9f6eae18ba1ebf112e903c2d69cc1c7d6c8ea68
muhammadsadiqabubakar09 <muhammadsadiqabubakar09@gmail.com>
2026-08-08 14:44:03 +0100

    Upgrade risk_manager.py to V3
    Added ATR-based stop loss, dynamic take profit, leverage suggestion and improved risk management.

--- risk_manager.py
+++ risk_manager.py
@@ -1,21 +1,34 @@
 from typing import Dict
 
 
-def calculate_trade(price: float, signal: str) -> Dict:
+def calculate_trade(price: float, atr: float, signal_data: Dict):
 
-    if signal == "BUY":
+    direction = signal_data["direction"]
+    confidence = signal_data["confidence"]
 
-        stop_loss = price * 0.98
-        tp1 = price * 1.02
-        tp2 = price * 1.04
-        tp3 = price * 1.06
+    if direction == "BUY":
 
-    elif signal == "SELL":
+        stop_loss = price - (1.5 * atr)
 
-        stop_loss = price * 1.02
-        tp1 = price * 0.98
-        tp2 = price * 0.96
-        tp3 = price * 0.94
+        tp1 = price + (2 * atr)
+        tp2 = price + (3 * atr)
+        tp3 = price + (4 * atr)
+
+    elif direction == "LONG":
+
+        stop_loss = price - (1.5 * atr)
+
+        tp1 = price + (2 * atr)
+        tp2 = price + (3 * atr)
+        tp3 = price + (4 * atr)
+
+    elif direction == "SHORT":
+
+        stop_loss = price + (1.5 * atr)
+
+        tp1 = price - (2 * atr)
+        tp2 = price - (3 * atr)
+        tp3 = price - (4 * atr)
 
     else:
         return {}
@@ -23,13 +36,39 @@ def calculate_trade(price: float, signal: str) -> Dict:
     risk = abs(price - stop_loss)
     reward = abs(tp2 - price)
 
-    rr = round(reward / risk, 2)
+    risk_percent = round((risk / price) * 100, 2)
+    reward_percent = round((reward / price) * 100, 2)
+
+    risk_reward = round(reward / risk, 2)
+
+    # Suggested Leverage
+    if confidence == "VERY HIGH":
+        leverage = "3x"
+
+    elif confidence == "HIGH":
+        leverage = "4x"
+
+    elif confidence == "MEDIUM":
+        leverage = "5x"
+
+    else:
+        leverage = "-"
 
     return {
-        "entry": round(price, 4),
-        "stop_loss": round(stop_loss, 4),
-        "tp1": round(tp1, 4),
-        "tp2": round(tp2, 4),
-        "tp3": round(tp3, 4),
-        "risk_reward": rr,
+
+        "entry": round(price, 6),
+
+        "stop_loss": round(stop_loss, 6),
+
+        "tp1": round(tp1, 6),
+        "tp2": round(tp2, 6),
+        "tp3": round(tp3, 6),
+
+        "risk_percent": risk_percent,
+        "reward_percent": reward_percent,
+
+        "risk_reward": risk_reward,
+
+        "suggested_leverage": leverage
+
     }
